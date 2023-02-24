a991e12bf5de9b23e3d6c9012547854d0d22b60c
AmitxParmar <amitparmar901@gmail.com>
2023-02-24 23:35:03 +0530

    complete hello_world in Django fix:DIR to templates

--- hello_world_project/urls.py
+++ hello_world_project/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 # imported views
-from my_app import views
 
 urlpatterns = [
+    path('', include('my_app.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index, name="homepage"),
 ]
